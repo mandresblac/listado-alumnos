a98be9d0e432aba468084175a3115ea2a96448e6
manuel blanco <mandresblac@gmail.com>
2024-04-19 17:44:37 -0500

    commit8 creacion formulario inscripcion

--- backend-listado-alumnos/src/models/alumno.model.ts
+++ backend-listado-alumnos/src/models/alumno.model.ts
@@ -3,13 +3,14 @@ import { Model, Schema, model } from "mongoose";
 const AlumnoSchema = new Schema({
   nombre: {type: String, required: true},
   apellido: {type: String, required: true},
+  email: { type: String, required: true, unique: true },
   genero: {type: String, required: true},
   edad: {type: Number, required: true},
   fechaNacimiento: {type: Date, required: true}, //
   direccion: {type: String, required: true},
-  telefono: {type: Number, required: true},
+  telefono: {type: String, required: true},
   tipoDocumento: { type: String, required: true},
-  numeroDocumento: {type: Number, required: true},
+  numeroDocumento: {type: String, required: true},
   createdAt: {type: Date, default: Date.now()},
   updatedAt: {type: Date, default: Date.now()}
 });
--- frontend-listado-alumnos/src/app/pages/inscripciones/inscripciones.component.ts
+++ frontend-listado-alumnos/src/app/pages/inscripciones/inscripciones.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inscripciones',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inscripciones.component.html',
   styleUrl: './inscripciones.component.css'
 })
 export class InscripcionesComponent {
-
+  alumnoForm = new FormGroup({
+    // Validaciones del formulario
+    nombre: new FormControl(),
+    apellido: new FormControl(),
+    email: new FormControl(),
+    genero: new FormControl(),
+    edad: new FormControl(),
+    fechaNacimiento: new FormControl(), //
+    direccion: new FormControl(),
+    telefono: new FormControl(),
+    tipoDocumento: new FormControl(),
+    numeroDocumento: new FormControl(),
+  })
 }
