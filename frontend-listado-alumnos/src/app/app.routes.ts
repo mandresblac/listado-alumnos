9328295c41cd32209b68960549f438bec5ebf90a
manuel blanco <mandresblac@gmail.com>
2024-04-18 20:05:53 -0500

    commit4 generacion de rutas en el frontend

--- frontend-listado-alumnos/src/app/app.routes.ts
+++ frontend-listado-alumnos/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InscripcionesComponent } from './pages/inscripciones/inscripciones.component';
+import { ListadoComponent } from './pages/listado/listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    title: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "inscripciones",
+    title: "inscripciones",
+    component: InscripcionesComponent,
+  },
+  {
+    path: "listado",
+    title: "listado",
+    component: ListadoComponent,
+  },
+  {
+    path: "**", redirectTo: "", pathMatch: "full"
+  }
+];
